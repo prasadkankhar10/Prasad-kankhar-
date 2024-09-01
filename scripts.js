e2c1010802cd4fe7416f8b6f1923b960f70727f6
prasadkankhar10 <prasadkankhar05@gamil.com>
2024-09-02 00:29:12 +0530

    Updated project: Added icons in HTML, applied new styles in CSS, and made JavaScript adjustments

--- scripts.js
+++ scripts.js
@@ -23,8 +23,8 @@ let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
 let timeDom = document.querySelector('.carousel .time');
 
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 3000;
-let timeAutoNext = 7000;
+let timeRunning = 2000;
+let timeAutoNext = 10000;
 
 nextDom.onclick = function(){
     showSlider('next');    
